52fb8e32351bca17f7281d568f6b7a0ebd72fabe
GYAN224143 <gyan224143@gmail.com>
2024-05-09 00:12:00 +0530

    add some info

---
+++ src/app/components/Gyan.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Gyan = () => {
+  return (
+    <div>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+      <h1>this is gyan project</h1>
+    </div>
+  )
+}
+
+export default Gyan
